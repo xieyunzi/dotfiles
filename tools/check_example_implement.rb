7034824e9bccc2d8a723fb31a0c1507e68e1cda6
xieyunzi <xieyunzi@foxmail.com>
2015-10-12 00:27:50 +0800

    update example implement check

--- tools/check_example_implement.rb
+++ tools/check_example_implement.rb
@@ -1,21 +1,29 @@
-class ExampleCheck
+class ImplementFile
   EXT_NAME = '.example'
 
   def initialize(full_path)
     @full_path = full_path
+    @implement_full_path =
+      full_path.sub(/#{Regexp.escape(EXT_NAME)}$/, '')
   end
 
   def implement?
-    implement_full_path = @full_path.sub(/#{Regexp.escape(EXT_NAME)}$/, '')
-    File.exist? implement_full_path
+    File.exist? @implement_full_path
+  end
+
+  def out_of_date?
+    File.mtime(@full_path).to_i - File.mtime(@implement_full_path).to_i > 0
   end
 end
 
 result =
-  Dir["#{Dir.home}/.*#{ExampleCheck::EXT_NAME}"].map do |full_path|
-    example_check = ExampleCheck.new full_path
-    check = example_check.implement? ? 'implemented' : 'none'
-    [full_path, check]
+  Dir["#{Dir.home}/.*#{ImplementFile::EXT_NAME}"].map do |full_path|
+    implement_file = ImplementFile.new full_path
+
+    is_implemented = implement_file.implement? ? 'implemented' : 'not_implemented'
+    is_out_of_date = implement_file.out_of_date? ? 'out_of_date' : 'up_to_date'
+
+    [full_path, is_implemented, is_out_of_date]
   end
 
 puts result
