af52eaadbd1e91ef10e6f627a4ae8c1143dfbbe2
xieyunzi <xieyunzi@foxmail.com>
2015-10-12 12:18:39 +0800

    refactor check example implement

--- tools/check_example_implement.rb
+++ tools/check_example_implement.rb
@@ -3,27 +3,35 @@ class ImplementFile
 
   def initialize(full_path)
     @full_path = full_path
+    @real_path = File.realpath(@full_path)
     @implement_full_path =
-      full_path.sub(/#{Regexp.escape(EXT_NAME)}$/, '')
+      @full_path.sub(/#{Regexp.escape(EXT_NAME)}$/, '')
   end
 
   def implement?
     File.exist? @implement_full_path
   end
 
-  def out_of_date?
-    File.mtime(@full_path).to_i - File.mtime(@implement_full_path).to_i > 0
+  def up_to_date?
+    File.mtime(@implement_full_path).to_i - File.mtime(@real_path).to_i > 0
+  end
+
+  def report
+    is_implemented = implement?
+    is_up_to_date = is_implemented && up_to_date?
+
+    {
+      full_path: @full_path,
+      real_path: @real_path,
+      implemented: is_implemented,
+      up_to_date: is_up_to_date
+    }
   end
 end
 
 result =
   Dir["#{Dir.home}/.*#{ImplementFile::EXT_NAME}"].map do |full_path|
-    implement_file = ImplementFile.new full_path
-
-    is_implemented = implement_file.implement? ? 'implemented' : 'not_implemented'
-    is_out_of_date = implement_file.out_of_date? ? 'out_of_date' : 'up_to_date'
-
-    [full_path, is_implemented, is_out_of_date]
+    ImplementFile.new(full_path).report.to_a.map { |r| r.to_s }
   end
 
 puts result
