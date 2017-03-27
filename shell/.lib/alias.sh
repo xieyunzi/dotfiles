f4c69aa75b74aa45479feee0d0b9118b096f0292
xieyunzi <xieyunzi@foxmail.com>
2017-03-27 12:23:34 +0800

    update docker alias, vim plug

--- shell/.lib/alias.sh
+++ shell/.lib/alias.sh
@@ -31,9 +31,24 @@ alias ls-depth-detail='find . -mindepth 2 -maxdepth 2 -type d -exec ls -ld "{}" 
 if [[ -x $(which hub) ]]; then alias g=hub; else alias g=git; fi
 alias tm=tmux
 
+# for docker {{{
 alias dr=docker
-alias drc=docker-compose
 alias drm=docker-machine
+
+alias dco='docker-compose'
+
+alias dcb='docker-compose build'
+alias dce='docker-compose exec'
+alias dcps='docker-compose ps'
+alias dcrestart='docker-compose restart'
+alias dcrm='docker-compose rm'
+alias dcr='docker-compose run'
+alias dcstop='docker-compose stop'
+alias dcup='docker-compose up'
+alias dcdn='docker-compose down'
+alias dcl='docker-compose logs'
+alias dclf='docker-compose logs -f'
+# }}}
 
 # for vim {{{
 
