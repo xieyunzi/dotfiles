070a1103f29382a7120f575aa3e7a8895e72a87b
xieyunzi <xieyunzi@foxmail.com>
2018-03-25 19:42:33 +0800

    update ssl, vim plug

--- shell/.lib/path.macosx.sh
+++ shell/.lib/path.macosx.sh
@@ -31,6 +31,15 @@ fi
 # haskell
 export HASKELL_HOME="$HOME/Library/Haskell"
 
+# openssl
+if [[ -e /usr/local/opt/openssl ]]; then
+  export PATH="/usr/local/opt/openssl/bin:$PATH"
+fi
+# libressl
+if [[ -e /usr/local/opt/libressl ]]; then
+  export PATH="/usr/local/opt/libressl/bin:$PATH"
+fi
+
 if [[ -e "$HOME/.lib/path.macosx.local" ]]; then . "$HOME/.lib/path.macosx.local"; fi
 
 fi
