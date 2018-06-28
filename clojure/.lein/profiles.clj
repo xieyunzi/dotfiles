e664649b1f1e551205cb9ca15d485d231451bf19
xieyunzi <xieyunzi@foxmail.com>
2018-06-28 14:43:40 +0800

    clojure

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,7 +1,11 @@
-{:user {:plugins [[lein-pprint "1.1.2"]
-                  [cider/cider-nrepl "0.18.0-SNAPSHOT"]
-                  [refactor-nrepl "2.4.0-SNAPSHOT"]
-                  [lein-kibit "0.1.6-beta2"]
-                  [jonase/eastwood "0.2.4"]
-                  ]
-        :dependencies [[slamhound "1.3.1"]]}}
+{:user
+ {:plugins
+  [[lein-pprint "1.2.0"]
+   [cider/cider-nrepl "0.18.0-SNAPSHOT"]
+   [refactor-nrepl "2.4.0-SNAPSHOT"]
+   [lein-kibit "0.1.6"]
+   [jonase/eastwood "0.2.6"]
+   ]
+  :dependencies
+  [[org.clojure/tools.nrepl "0.2.12"]
+   ]}}
