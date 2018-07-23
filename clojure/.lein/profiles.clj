fe4ba3ac7fb4cdaf87af2fd4fd2c74d9de7d0a5d
xieyunzi <xieyunzi@foxmail.com>
2018-07-23 19:06:27 +0800

    emacs, clojure

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user
  {:plugins
   [[lein-pprint "1.2.0"]
-   [cider/cider-nrepl "0.18.0-SNAPSHOT"]
+   [cider/cider-nrepl "0.17.0"]
    [refactor-nrepl "2.4.0-SNAPSHOT"]
    [lein-kibit "0.1.6"]
    [jonase/eastwood "0.2.6"]
