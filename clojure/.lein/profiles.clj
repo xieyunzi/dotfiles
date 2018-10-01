2e57e482c591ca3f0cfd84e928f332e815e11fed
xieyunzi <xieyunzi@foxmail.com>
2018-10-01 23:43:21 +0800

    clojure

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,11 +1,11 @@
 {:user
  {:plugins
   [[lein-pprint "1.2.0"]
-   [cider/cider-nrepl "0.17.0"]
-   [refactor-nrepl "2.4.0-SNAPSHOT"]
+   [cider/cider-nrepl "0.18.0"]
+   [refactor-nrepl "2.4.0"]
    [lein-kibit "0.1.6"]
    [jonase/eastwood "0.2.6"]
    ]
   :dependencies
-  [[org.clojure/tools.nrepl "0.2.12"]
+  [[org.clojure/tools.nrepl "0.2.13"]
    ]}}
